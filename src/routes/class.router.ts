6146c8ceeb53e2747897b560ab9dfdffe6b4c422
herminioneto <herminio_neto@outlook.com>
2025-01-03 12:06:40 -0300

    feat: implementação inicial do classroom controller e classroom router

---
+++ src/routes/class.router.ts
@@ -0,0 +1,54 @@
+import { BaseRouter } from "./base.router";
+import type { FastifyInstance } from "fastify";
+import { ClassroomController } from "../controllers/classroom.controllers";
+import { ClassMinimalSchema } from "../schemas/class.schemas";
+
+export class ClassroomRouter extends BaseRouter {
+  classroomController: ClassroomController;
+
+  constructor(app: FastifyInstance) {
+    super(app, "Classroom Routes", "/classroom/");
+    this.classroomController = new ClassroomController();
+  }
+
+  registerRoutes(): void {
+    this.addListClassroomRoute();
+    this.addCreateClassroomRoute();
+  }
+
+  addListClassroomRoute() {
+    this.constructRoute({
+      url: this.prefix,
+      method: "GET",
+      schema: {
+        tags: ["classroom"],
+        response: { 200: ClassMinimalSchema.array() },
+      },
+      handler: this.classroomController.getAllClasses,
+    });
+  }
+
+  addGetClassroomRoute() {
+    this.constructRoute({
+      url: `${this.prefix}:id`,
+      method: "GET",
+      schema: {
+        tags: ["classroom"],
+        response: { 200: ClassMinimalSchema },
+      },
+      handler: this.classroomController.getClassroomById,
+    });
+  }
+
+  addCreateClassroomRoute() {
+    this.constructRoute({
+      url: this.prefix,
+      method: "POST",
+      schema: {
+        tags: ["classroom"],
+        response: { 201: ClassMinimalSchema },
+      },
+      handler: this.classroomController.createClassroom,
+    });
+  }
+}
